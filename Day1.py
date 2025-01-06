96d824b99833d6b404503c9a7104bef47d14292a
Asemanijoshua <joshuaasemani27@gmail.com>
2025-01-06 11:54:39 +0000

    Finished Day 1

---
+++ Day1.py
@@ -0,0 +1,14 @@
+# Basic Python Commands
+
+# Printing Hello, World
+print("Hello, World!")
+# Adding and Displaying the result
+print(2+2)
+# Multiplying and Displaying the result
+print(7*6)
+# Subtracting and Displaying the result
+print(2-1)
+# Dividing and Displaying the result
+print(16/2)
+# Making Integer Division and Displaying the result
+print(13//2)
