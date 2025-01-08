da2e4fe073d27cf4cbb052567e44e288e4270348
Asemanijoshua <joshuaasemani27@gmail.com>
2025-01-08 09:52:51 +0000

    Completed Day 3

---
+++ Day3.py
@@ -0,0 +1,10 @@
+#Conditional Statements
+
+#Simple Age Checker
+#Getting User input
+Age = int(float(input("Enter your age: ")))
+
+if Age >= 18:
+    print("You are eligible to vote 👌")
+else:
+    print("You are too young to vote 😡")
