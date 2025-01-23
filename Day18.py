3166f81dc57220ab657eedefbc6ae1db13e65a35
Asemanijoshua <joshuaasemani27@gmail.com>
2025-01-23 16:51:35 +0000

    Completed Day 18

---
+++ Day18.py
@@ -0,0 +1,6 @@
+# Day 18: Introduction to Cryptography
+# - Topics:
+# - Introduction to hashing with the hashlib library.
+# - Hashing algorithms: MD5, SHA1, SHA256.
+# - Project:
+# - Write a Python script that hashes a user’s password and compares it to a known hash.
