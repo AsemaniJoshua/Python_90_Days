d33f2bf8208ed1b8e0a091962615e7ffd3c47660
Asemanijoshua <joshuaasemani27@gmail.com>
2025-01-24 15:52:57 +0000

    Created Day 19 file

---
+++ Day19.py
@@ -0,0 +1,7 @@
+# Day 19-20: Introduction to Encryption
+# - Topics:
+# - Learn basic encryption concepts using the cryptography library.
+# - Symmetric vs. asymmetric encryption.
+# - Project:
+# - Create a simple encryption/decryption tool using the cryptography library (e.g., AES encryption)
+
