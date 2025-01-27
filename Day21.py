5346a1cbf21a4504f6ae1ae8b00ade4b69d919a2
Asemanijoshua <joshuaasemani27@gmail.com>
2025-01-27 23:28:08 +0000

    Still on Day 22 and 23

--- Day21.py
+++ Day21.py
@@ -1,8 +1,20 @@
-# Day 21
+# Day 21: Automating Tasks with Python
+# - Topics:
+# - Learn how to automate tasks using Python scripts.
+# - Use os and shutil for file manipulation.
+# - Project:
+# - Write a Python script that automatically organizes files in a folder by extension (e.g., move .txt files to
+# a "TextFiles" folder).
 
 
+# Importing Modules or Libraries
+import os
+import shutil
 
+# Creating a directory
+# os.makedirs("Destination_Folder", exist_ok=True)
 
+shutil.copy("Destination_Folder/File_to_Move.txt","File_to_Move.txt")
 
 
 
