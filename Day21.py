cd388794f0c9897d3c54203fc5d8e0ec70d05cd0
Asemanijoshua <joshuaasemani27@gmail.com>
2025-01-30 23:31:46 +0000

    Starting Day 24 to 25

--- Day21.py
+++ Day21.py
@@ -36,3 +36,51 @@ import shutil
 #     else:
 #         os.makedirs(f"{path}/{extension}")
 #         shutil.move(f"{path}/{file}", f"{path}/{extension}/{file}")
+
+
+
+# Using the dictionary method
+
+directory = input("Enter hte path of the folder you want to organize its files: ")
+
+file_types = {
+    "images" : [".jpg", ".jpeg", ".png", ".webp"],
+    "documents": [".docx", ".txt", ".ppx", ".pdf"],
+    "videos" : [".mp4", ".wav"],
+    "audios" : [".mp3"],
+    "Spreadsheets" : [".xls", ".xlsx"],
+    "archives" : [".zip", ".rar", ".7zip"]
+}
+
+# FUnction to create FOlders to store files based on their 
+def create_folders(base_dir, folders):
+    for folder in folders:
+        folder_path = os.path.join(base_dir,folder)
+        if not os.path.exists(folder_path):
+            os.makedirs(folder_path)
+            
+            
+            
+            
+# Function to sort the files into the folders based on their file extension
+def organize_files(base_dir, file_types):
+    for filename in os.listdir(base_dir):
+        file_path = os.path.join(base_dir,filename)
+        
+        if os.path.isdir(file_path):
+            continue
+        
+        name, ext = os.path.splitext(filename)
+        
+        for folder,extensions in file_types.items():
+            if ext in extensions:
+                target_folder = os.path.join(base_dir,folder)
+                shutil.move(file_path,target_folder)
+                break
+            
+            
+            
+            
+# Calling both the create_folder function and the organize_files function
+create_folders(directory,file_types.keys())
+organize_files(directory,file_types)
---
+++ Day24-25.py
@@ -0,0 +1,6 @@
+# Day 24-25: Brute Force Password Cracking
+# - Topics:
+# - Learn how brute force attacks work.
+# - Introduction to itertools for generating combinations.
+# - Project:
+# - Write a script that attempts to brute-force a password using a wordlist and hashes it with hashlib.
